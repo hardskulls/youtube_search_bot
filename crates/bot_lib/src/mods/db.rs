77a96210077b59d39075a1152bdf5fa3c9b72f39
alexusic <notalexusic@gmail.com>
2022-12-28 18:10:30 +0300

    #[style (db)] ✨ Changed log format

--- crates/bot_lib/src/mods/db.rs
+++ crates/bot_lib/src/mods/db.rs
@@ -6,14 +6,14 @@ pub(crate) fn get_access_token(user_id: &str, redis_url: &str) -> eyre::Result<Y
 {
     log::info!("getting access_token from a database | (silent on failure)");
     let client = redis::Client::open(redis_url);
-    log::info!(" [:: LOG ::] ... : ( @:[fn::get_access_token] 'client' is [| '{:#?}' |] )", &client);
+    log::info!("[:: LOG ::]    ( @:[fn::get_access_token] 'client' is [| '{:#?}' |] )", &client);
     let client = client?;
     let mut con = client.get_connection()?;
     let serialized_access_token = con.get::<_, String>(user_id);
-    log::info!(" [:: LOG ::] ... : ( @:[fn::get_access_token] 'serialized_access_token' is [| '{:#?}' |] )", &serialized_access_token);
+    log::info!("[:: LOG ::]    ( @:[fn::get_access_token] 'serialized_access_token' is [| '{:#?}' |] )", &serialized_access_token);
     let serialized_access_token = serialized_access_token?;
     let youtube_access_token = serde_json::from_str::<YouTubeAccessToken>(&serialized_access_token);
-    log::info!(" [:: LOG ::] ... : ( @:[fn::get_access_token] 'youtube_access_token' is [| '{:#?}' |] )", &youtube_access_token);
+    log::info!("[:: LOG ::]    ( @:[fn::get_access_token] 'youtube_access_token' is [| '{:#?}' |] )", &youtube_access_token);
     let youtube_access_token = youtube_access_token?;
     log::info!("access_token acquired!");
     Ok(youtube_access_token)
