0faf058a359dc8eccbf6fbb85d982328e8d1c8f4
alexusic <notalexusic@gmail.com>
2023-04-05 22:00:14 +0300

    #[🩹fix (app)] Trying to make logs to work; Switched back to `simple_logger`

--- crates/app/src/bin/auth_server/main.rs
+++ crates/app/src/bin/auth_server/main.rs
@@ -13,23 +13,23 @@ use internal::auth_server::{handle_auth_code, serve_all};
 #[tokio::main]
 async fn main() -> eyre::Result<()>
 {
-    env_logger::Builder::new()
-        .format
-        (
-            |buf, record|
-                writeln!
-                (
-                    buf,
-                    " [:: LOG ::] : [{}:{}] : [{}] [{}] :: ({}) ",
-                    record.file().unwrap_or("unknown"),
-                    record.line().unwrap_or(0),
-                    chrono::Local::now().format("%Y-%m-%dT%H:%M:%S"),
-                    record.level(),
-                    record.args()
-                )
-        )
-        .init();
-    //simple_logger::init_with_env().or_else(|_| simple_logger::init_with_level(log::Level::Info))?;
+    //env_logger::Builder::new()
+    //    .format
+    //    (
+    //        |buf, record|
+    //            writeln!
+    //            (
+    //                buf,
+    //                " [:: LOG ::] : [{}:{}] : [{}] [{}] :: ({}) ",
+    //                record.file().unwrap_or("unknown"),
+    //                record.line().unwrap_or(0),
+    //                chrono::Local::now().format("%Y-%m-%dT%H:%M:%S"),
+    //                record.level(),
+    //                record.args()
+    //            )
+    //    )
+    //    .init();
+    simple_logger::init_with_env().or_else(|_| simple_logger::init_with_level(log::Level::Info))?;
     
     log::info!(" [:: LOG ::]    ( ⚙ <| Building 'auth_server'... |> ⚙ )");
     let router : Router =
--- crates/app/src/bin/bot/main.rs
+++ crates/app/src/bin/bot/main.rs
@@ -28,23 +28,23 @@ use internal::handlers::{handle_callback, handle_commands, handle_text, handle_u
 #[tokio::main]
 async fn main() -> eyre::Result<()>
 {
-    env_logger::Builder::new()
-        .format
-        (
-            |buf, record|
-                writeln!
-                (
-                    buf,
-                    " [:: LOG ::] : [{}:{}] : [{}] [{}] :: ({}) ",
-                    record.file().unwrap_or("unknown"),
-                    record.line().unwrap_or(0),
-                    chrono::Local::now().format("%Y-%m-%dT%H:%M:%S"),
-                    record.level(),
-                    record.args()
-                )
-        )
-        .init();
-    //simple_logger::init_with_env().or_else(|_| simple_logger::init_with_level(log::Level::Info))?;
+    //env_logger::Builder::new()
+    //    .format
+    //    (
+    //        |buf, record|
+    //            writeln!
+    //            (
+    //                buf,
+    //                " [:: LOG ::] : [{}:{}] : [{}] [{}] :: ({}) ",
+    //                record.file().unwrap_or("unknown"),
+    //                record.line().unwrap_or(0),
+    //                chrono::Local::now().format("%Y-%m-%dT%H:%M:%S"),
+    //                record.level(),
+    //                record.args()
+    //            )
+    //    )
+    //    .init();
+    simple_logger::init_with_env().or_else(|_| simple_logger::init_with_level(log::Level::Info))?;
 
     log::info!("[ LOG ] ⚙ <| Building bot... |>");
     log::info!("[ LOG ] 📝 <| Command description: {} |>", Command::descriptions());
