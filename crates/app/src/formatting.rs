288fa43a315b5cbef4f96be91f658c35cab0e8a9
Alexander Lebedev <hardskulls@rambler.ru>
2023-07-23 20:10:03 +0300

    #[::✨style (app/formatting)::] Updated logs formatting.

--- crates/app/src/formatting.rs
+++ crates/app/src/formatting.rs
@@ -6,12 +6,24 @@ use log::Record;
 
 pub fn format_logs(buf: &mut Formatter, record: &Record) -> std::io::Result<()>
 {
-
-    let (file, line) = (record.file().unwrap_or("unknown file"), record.line().unwrap_or(0));
-    let time = chrono::Local::now().format("Date %Y.%m.%d | Time %H:%M:%S");
+    let file = record.file().unwrap_or("unknown file");
+    let line = record.line().unwrap_or(0);
+    let time = chrono::Local::now().format("DATE ~ %Y/%m/%d || TIME ~ %H:%M:%S");
     let (level, args) = (record.level(), record.args());
-
-    writeln!(buf, "[ LOG : {level} ] \n   ->   [ {file}:{line} ] \n   ->   [ {time} ] \n   ->   ( {args} ) \n\n\n\n\n ")
+    let separator = "===================================================================";
+    
+    writeln!
+    (
+        buf,
+        "\
+        \n{separator} \
+        \n\nLOG : {level} \
+        \n   ->   LOGGED AT ~ {file}:{line} \
+        \n   ->   {time} \
+        \n\n{args} \
+        \n\n{separator}\n\n\n \
+        "
+    )
 }
 
 
